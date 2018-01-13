c3803c2089d2d647690bf9ad28a846859ac48b4d
meta <lio114514@gmail.com>
2018-01-13 23:23:32 +0900

    variable

--- variable.go
+++ variable.go
@@ -1,17 +1,17 @@
 package main
 
-import(
+import (
 	"fmt"
 )
 
-func main(){
-	a := 5          // int
-	b := 1.35       // float
-	c := "hoge"     // string
-	var d bool      // bool
+func main() {
+	a := 5         // int
+	b := 1.35      // float
+	c := "hoge"    // string
+	var d bool     // bool
 	const e = "定数" // 定数
 
-	fmt.Printf("a:%d b:%f c:%s d:%t e:%s \n", a,b,c,d,e)
+	fmt.Printf("a:%d b:%f c:%s d:%t e:%s \n", a, b, c, d, e)
 
 	// 定数列挙
 	// 識別子iota(イオタ)を使用することで0から始まる連番にできる
