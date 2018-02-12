ca922c105eba9d4868ea4ae114df64681a603d87
meta <lio114514@gmail.com>
2018-02-12 16:06:27 +0900

    method_abstract_ex

--- method_abstract.go
+++ method_abstract.go
@@ -23,6 +23,13 @@ func (h *Human) SayHi(){
 	fmt.Printf("Hi, I am %s you can call me on %s\n", h.name, h.phone)
 }
 
+// EmployeeのmethodでHumanのmethodを書き直す
+func (e *Employee) SayHi(){
+	fmt.Printf("Hi, I am %s, I work at %s. Call me on %s\n", e.name,
+		e.company, e.phone) //Yes you can split into 2 lines here.
+}
+
+
 func main() {
 	mark := Student{Human{"Mark",25,"080-1111-2222"}, "MIT"}
 	sam := Employee{Human{"Sam",45,"111-888-XXXX"}, "Go Inc"}
